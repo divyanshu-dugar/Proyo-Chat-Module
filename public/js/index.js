a598c8a639cbf3d0eaf771c84fd5dc15916bf55b
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-24 12:27:26 -0500

    DOM JS Code

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+const socket = io('http://localhost:3000');
+const chat = document.getElementById('chat');
+const usernameInput = document.getElementById('usernameInput');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+
+// Load chat history from server
+fetch('http://localhost:3000/chats')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(chat => {
+            addMessage(chat.username, chat.message);
+    });
+});
+
+// Add message to the chat box
+function addMessage(username, message) {
+    const div = document.createElement('div');
+    div.className = 'message';
+    div.innerHTML = `<span class="username">${username}:</span> <i> ${message} </i>`;
+    chat.appendChild(div);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+// Send message on button click
+sendButton.addEventListener('click', () => {
+    const username = usernameInput.value;
+    const message = messageInput.value;
+    if (username && message) {
+        socket.emit('sendMessage', { username, message });
+        messageInput.value = '';
+    }
+});
+
+// Receive new message from server
+socket.on('receiveMessage', (data) => {
+    addMessage(data.username, data.message);
+});
